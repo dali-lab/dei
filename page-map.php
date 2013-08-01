c4129428e6645c81f1ed3fb52b59b58eed1ff6d5
Student <student@Waynes-MacBook-Air.local>
2013-08-01 18:14:53 -0400

    map

--- page-map.php
+++ page-map.php
@@ -5,3 +5,24 @@ Template Name: Map Page
 ?>
 
 <?php get_header(); ?>
+
+
+ <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+    <script>
+var map;
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+    </script>
+
+    
+<?php get_footer(); ?>
