fec648f3afef799c1f30d33a17f3e7c6d193795f
Student <student@dhcp-235-93.wireless.dartmouth.edu>
2013-08-05 13:19:20 -0400

    map

--- page-map.php
+++ page-map.php
@@ -8,10 +8,14 @@ Template Name: Map Page
 
 <?php get_header(); ?>
 
-     
-
-  <div id="main-content"<?php if ($fullwidth) echo ' class="fullwidth"'; ?>>
-  	<head>
+   <div id="main-content" class="fullwidth">
+	<div class="container clearfix">
+		<div id="entries-area">
+			<div id="entries-area-inner">
+				<div id="entries-area-content" class="clearfix">
+					<div id="content-area">
+											
+						<head>
   	<style>
       #map_canvas {
         width: 500px;
@@ -35,8 +39,16 @@ Template Name: Map Page
 
  </head>
 <div id="map_canvas"></div>
-   
 
+																</div> <!-- end #content-area -->
+
+				</div> <!-- end #entries-area-content -->
+			</div> <!-- end #entries-area-inner -->
+		</div> <!-- end #entries-area -->
+	</div> <!-- end .container -->
 </div> <!-- end #main-content -->
+
+
+
 
 <?php get_footer(); ?>
