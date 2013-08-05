b759a155b7ef2e729de0c005c8bf537a436b47d1
Student <student@dhcp-235-93.wireless.dartmouth.edu>
2013-08-05 13:52:46 -0400

    map

--- map.php
+++ map.php
@@ -9,10 +9,10 @@ Template Name: mapzzz
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
     <style>
-      html, body, #map-canvas {
-        margin: 0;
-        padding: 0;
-        height: 100%;
+      html, body, #map_canvas {
+        width: 500px;
+        height: 400px;
+      }
       }
     </style>
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
