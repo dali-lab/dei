635df0359db571be4fe78edddbf5f1d0497f7ba2
Student <student@dhcp-235-93.wireless.dartmouth.edu>
2013-08-05 14:00:09 -0400

    map

--- map.php
+++ map.php
@@ -2,37 +2,31 @@
 /*
 Template Name: mapzzz
 */
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html>
   <head>
-    <title>Simple Map</title>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
     <style>
-      html, body, #map_canvas {
+      #map_canvas {
         width: 500px;
         height: 400px;
       }
-      }
     </style>
-    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+    <script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
     <script>
-var map;
-function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
+      function initialize() {
+        var map_canvas = document.getElementById('map_canvas');
+        var map_options = {
+          center: new google.maps.LatLng(44.5403, -78.5463),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(map_canvas, map_options)
+      }
+      google.maps.event.addDomListener(window, ‘load’, initialize);
     </script>
   </head>
   <body>
-    <div id="map-canvas"></div>
+    <div id="map_canvas"></div>
   </body>
 </html>
