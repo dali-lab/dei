1712e8082bba727ca6c51bf1293966307d9e2a7e
Student <student@Waynes-MacBook-Air.local>
2013-08-06 00:28:01 -0400

    map

--- page-map.php
+++ page-map.php
@@ -20,7 +20,7 @@ Template Name: Map Page
   <script src="http://maps.google.com/maps/api/js?sensor=false"  type="text/javascript"></script>
 
 
- <div id="romeluv-global-map" style="width: 100%; height:320px;  "></div>
+ <div id="romeluv-global-map" style="width: 960px; height:520px;  "></div>
           
             <script type="text/javascript">
            
