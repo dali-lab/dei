6c52dc3e51b6dd25762c04fc45beb219bd0c62ba
Student <student@dhcp-221-211.wireless.dartmouth.edu>
2013-08-09 14:12:33 -0400

    map

--- page-map.php
+++ page-map.php
@@ -9,10 +9,6 @@ Template Name: Map Page
 <?php get_header(); ?>
 
    <div id="main-content" class="fullwidth">
-	<div class="container clearfix">
-		<div id="entries-area">
-			<div id="entries-area-inner">
-				<div id="entries-area-content" class="clearfix">
 					<div id="content-area">
 
 
@@ -54,10 +50,7 @@ Template Name: Map Page
 
 																</div> <!-- end #content-area -->
 
-				</div> <!-- end #entries-area-content -->
-			</div> <!-- end #entries-area-inner -->
-		</div> <!-- end #entries-area -->
-	</div> <!-- end .container -->
+
 </div> <!-- end #main-content -->
 
 
