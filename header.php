fa580ef83084a510f57e0a123c9582cabf8a1f27
Student <student@Waynes-MacBook-Air.local>
2013-08-10 22:19:50 -0400

    header

--- header.php
+++ header.php
@@ -1,80 +1,143 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-<head profile="http://gmpg.org/xfn/11">
-<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-<title><?php elegant_titles(); ?></title>
-<?php elegant_description(); ?>
-<?php elegant_keywords(); ?>
-<?php elegant_canonical(); ?>
-
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
-<link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>" />
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
-
-<!--[if lt IE 7]>
-	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/ie6style.css" />
-	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/DD_belatedPNG_0.0.8a-min.js"></script>
-	<script type="text/javascript">DD_belatedPNG.fix('img#logo, span.menu_arrow, a#search img, #searchform, .featured-img span.overlay, #featured .video-slide, #featured a.join-today, div#controllers a, a.readmore span, #breadcrumbs-left, #breadcrumbs-right, #breadcrumbs-content, .span.overlay2, span.overlay, a.zoom-icon, a.more-icon');</script>
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if IE 7]>
-	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/ie7style.css" />
+<html id="ie7" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/ie8style.css" />
+<html id="ie8" <?php language_attributes(); ?>>
 <![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<title><?php elegant_titles(); ?></title>
+	<?php elegant_description(); ?>
+	<?php elegant_keywords(); ?>
+	<?php elegant_canonical(); ?>
 
-<script type="text/javascript">
-	document.documentElement.className = 'js';
-</script>
+	<?php do_action('et_head_meta'); ?>
 
-<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-<?php wp_head(); ?>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
+	<!--[if lt IE 9]>
+		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+	<![endif]-->
+
+	<script type="text/javascript">
+		document.documentElement.className = 'js';
+	</script>
+
+	<?php wp_head(); ?>
 </head>
-<body<?php if ( is_front_page() || is_home() ) echo ' id="home"'; ?> <?php body_class(); ?>>
-	<div id="center-highlight">
-		<div id="header">
-			<div class="container clearfix">
+<body <?php body_class(); ?>>
+	<?php do_action('et_header_top'); ?>
+	<header id="main-header">
+		<div class="container clearfix">
+			<?php do_action('et_header_menu'); ?>
+			<nav id="top-menu">
 				<?php
-					global $shortname, $default_colorscheme;
-					$colorSchemePath = '';
-					$colorScheme = get_option($shortname . '_color_scheme');
-					if ($colorScheme <> $default_colorscheme) $colorSchemePath = strtolower(esc_attr($colorScheme)) . '/';
+					$menuClass = 'nav';
+					if ( et_get_option('lucid_disable_toptier') == 'on' ) $menuClass .= ' et_disable_top_tier';
+					$primaryNav = '';
+					if (function_exists('wp_nav_menu')) {
+						$primaryNav = wp_nav_menu( array( 'theme_location' => 'primary-menu', 'container' => '', 'fallback_cb' => '', 'menu_class' => $menuClass, 'echo' => false ) );
+					}
+					if ($primaryNav == '') { ?>
+						<ul class="<?php echo esc_attr( $menuClass ); ?>">
+							<?php if (et_get_option('lucid_home_link') == 'on') { ?>
+								<li <?php if (is_home()) echo('class="current_page_item"') ?>><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e('Home','Lucid') ?></a></li>
+							<?php }; ?>
+
+							<?php show_page_menu($menuClass,false,false); ?>
+							<?php show_categories_menu($menuClass,false); ?>
+						</ul>
+					<?php }
+					else echo($primaryNav);
 				?>
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-					<?php $logo = (get_option('nova_logo') <> '') ? get_option('nova_logo') : get_template_directory_uri().'/images/'.$colorSchemePath.'logo.png'; ?>
-					<img src="<?php echo esc_attr($logo); ?>" alt="<?php echo esc_attr(get_bloginfo('name')); ?>" id="logo"/>
-				</a>
-				<?php do_action('et_header_menu'); ?>
-				<?php $menuClass = 'nav';
-				$menuID = 'top-menu';
-				$primaryNav = '';
-				if (function_exists('wp_nav_menu')) {
-					$primaryNav = wp_nav_menu( array( 'theme_location' => 'primary-menu', 'container' => '', 'fallback_cb' => '', 'menu_class' => $menuClass, 'menu_id' => $menuID, 'echo' => false ) );
-				};
-				if ($primaryNav == '') { ?>
-					<ul id="<?php echo esc_attr( $menuID ); ?>" class="<?php echo esc_attr( $menuClass ); ?>">
-						<?php if (get_option('nova_home_link') == 'on') { ?>
-							<li <?php if (is_home()) echo('class="current_page_item"') ?>><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e('Home','Nova') ?></a></li>
-						<?php }; ?>
+			</nav>
 
-						<?php show_page_menu($menuClass,false,false); ?>
+			<?php
+				$social_icons = '';
+				$et_rss_url = '' != et_get_option('lucid_rss_url') ? et_get_option('lucid_rss_url') : get_bloginfo('rss2_url');
+				if ( 'on' == et_get_option('lucid_show_twitter_icon') ) $social_icons['twitter'] = array('image' => get_bloginfo('template_directory') . '/images/twitter.png', 'url' => et_get_option('lucid_twitter_url'), 'alt' => 'Twitter' );
+				if ( 'on' == et_get_option('lucid_show_rss_icon') ) $social_icons['rss'] = array('image' => get_bloginfo('template_directory') . '/images/rss.png', 'url' => $et_rss_url, 'alt' => 'Rss' );
+				if ( 'on' == et_get_option('lucid_show_facebook_icon') ) $social_icons['facebook'] = array('image' => get_bloginfo('template_directory') . '/images/facebook.png', 'url' => et_get_option('lucid_facebook_url'), 'alt' => 'Facebook' );
+				$social_icons = apply_filters('et_social_icons', $social_icons);
+				if ( !empty($social_icons) ) {
+					echo '<div id="social-icons">';
+					foreach ($social_icons as $icon) {
+						echo "<a href='" . esc_url( $icon['url'] ) . "' target='_blank'><img alt='" . esc_attr( $icon['alt'] ) . "' src='" . esc_attr( $icon['image'] ) . "' /></a>";
+					}
+					echo '</div> <!-- end #social-icons -->';
+				}
+			?>
 
-						<?php show_categories_menu($menuClass,false); ?>
-					</ul> <!-- end ul#nav -->
-				<?php }
-				else echo($primaryNav); ?>
-
+			<div id="search">
 				<div id="search-form">
-					<a href="#" id="search"><img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $colorSchemePath; ?>search_btn.png" width="15" height="15" alt="Search" /></a>
-
-					<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-						<input type="text" value="<?php esc_attr_e('search this site...','Nova'); ?>" name="s" id="searchinput" />
+					<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>/">
+						<input type="text" value="<?php esc_attr_e('Search This Site...', 'Lucid'); ?>" name="s" id="searchinput" />
+						<input type="image" alt="<?php echo esc_attr( 'Submit', 'Lucid' ); ?>" src="<?php echo esc_url( get_template_directory_uri() . '/images/search_btn.png' ); ?>" id="searchsubmit" />
 					</form>
 				</div> <!-- end #search-form -->
-			</div> <!-- end .container -->
-		</div> <!-- end #header -->
+			</div> <!-- end #search -->
+		</div> <!-- end .container -->
+	</header> <!-- end #main-header -->
 
-		<?php if ( (is_front_page() || is_home()) && get_option('nova_featured') == 'on' ) get_template_part('includes/featured');?>
+	<?php
+		$use_header_banner = et_get_option( 'lucid_468_header_enable', 'false' );
+	?>
+
+	<div class="container">
+		<div id="logo-area"<?php if ( 'on' == $use_header_banner ) echo ' class="header_banner clearfix"'; ?>>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+				<?php
+					$color_scheme = et_get_option( 'lucid_color_scheme', 'Orange' );
+					$color_scheme = ( 'Orange' == $color_scheme ) ? '' : '-' . strtolower( $color_scheme );
+					$logo = ( ( $user_logo = et_get_option('lucid_logo') ) && '' != $user_logo ) ? $user_logo : get_template_directory_uri() . "/images/logo{$color_scheme}.png";
+				?>
+				<img src="<?php echo esc_url ( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo('name')) ; ?>" id="logo"/>
+			</a>
+
+			<?php if ( 'on' == $use_header_banner ){ ?>
+				<div id="top_banner">
+					<?php
+						if ( ( $lucid_468_header_adsense = et_get_option('lucid_468_header_adsense') ) && '' != $lucid_468_header_adsense ) echo( $lucid_468_header_adsense );
+						else { ?>
+						   <a href="<?php echo esc_url(et_get_option('lucid_468_header_url')); ?>"><img src="<?php echo esc_attr(et_get_option('lucid_468_header_image')); ?>" /></a>
+					<?php } ?>
+				</div> <!-- end #top_banner -->
+			<?php } ?>
+		</div>
+	</div> <!-- end .container -->
+	<div id="secondary-menu">
+		<div class="container">
+			<?php do_action('et_secondary_menu'); ?>
+			<nav id="second-menu" class="clearfix">
+				<?php
+					$menuClass = 'nav';
+					if ( et_get_option('lucid_disable_toptier') == 'on' ) $menuClass .= ' et_disable_top_tier';
+					$primaryNav = '';
+					if (function_exists('wp_nav_menu')) {
+						$primaryNav = wp_nav_menu( array( 'theme_location' => 'secondary-menu', 'container' => '', 'fallback_cb' => '', 'menu_class' => $menuClass, 'echo' => false ) );
+					}
+					if ($primaryNav == '') { ?>
+						<ul class="<?php echo esc_attr( $menuClass ); ?>">
+							<?php if (et_get_option('lucid_home_link') == 'on') { ?>
+								<li <?php if (is_home()) echo('class="current_page_item"') ?>><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e('Home','Lucid') ?></a></li>
+							<?php }; ?>
+
+							<?php show_page_menu($menuClass,false,false); ?>
+							<?php show_categories_menu($menuClass,false); ?>
+						</ul>
+					<?php }
+					else echo($primaryNav);
+				?>
+			</nav>
+		</div> <!-- end .container -->
+	</div> <!-- end #secondary-menu -->
+	<div id="main-area">
+		<div class="container">
