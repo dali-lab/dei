6d46924e7f21a578fb5867c6c6fc55d49aacb2c0
Student <student@Waynes-MacBook-Air.local>
2013-08-12 00:58:39 -0400

    time

---
+++ page-history.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<?php
+/*
+Template Name: History Page
+*/
+?>
+
+<?php get_header(); ?>
+
+   <div id="main-content" class="fullwidth">
+
+<iframe src='http://embed.verite.co/timeline/?source=0AgSneynBZQwRdEN4NmJFdzZydFdIeWF4S0JXT3cwZFE&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650' width='100%' height='650' frameborder='0'></iframe>
+
+
+
+</div> <!-- end #main-content -->
+
+
+
+
+<?php get_footer(); ?>
