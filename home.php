42f337b26e1066b1c9d18c3e86a32a0404d6b033
Student <student@Waynes-MacBook-Air.local>
2013-08-12 16:07:50 -0400

    middle part

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
 
 <div id="category-name"> 
 
-					<p style="color:#21B000;text-align: center;font-size:50px;">What is the Dartmouth Energy Initiative?<br></p>
+					<p style="color:#21B000;text-align: center;font-size:50px;">What is the Dartmouth Energy Initiative?<br><br><br></p>
 					<p style="text-align:center;font-size:18px;">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."<br>
 "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</p>
             
