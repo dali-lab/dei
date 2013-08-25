f74f079aa7aad0756e21cbaaf3112c5a8012fd47
Robin Wang <robin@Robins-MacBook-Air.local>
2013-08-25 07:18:04 -0400

    tabs

--- page-map.php
+++ page-map.php
@@ -8,50 +8,47 @@ Template Name: Map Page
 
 <?php get_header(); ?>
 
-   <div id="main-content" class="fullwidth">
-
-
-
+<div id="main-content" class="fullwidth">
 
   <script src="http://maps.google.com/maps/api/js?sensor=false"  type="text/javascript"></script>
 
 
- <div id="romeluv-global-map" style="width:100%; height:520px; "></div>
-          
-            <script type="text/javascript">
-           
-              var map = new google.maps.Map(document.getElementById("romeluv-global-map"), {
-                zoom: 17,
-                
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-              });
-          
-              var infowindow = new google.maps.InfoWindow();
-            var bounds = new google.maps.LatLngBounds();
-              var marker, i;
-             var myIcon = new google.maps.MarkerImage("http://maps.google.com/mapfiles/kml/pal3/icon34.png", null, null, null, new google.maps.Size(30,30));
-                                  marker = new google.maps.Marker({
-                                    position: new google.maps.LatLng(52.53269103999427, 13.326115608215332),
-                                    map: map,
-                                    icon:  myIcon
-                                  });
-                                  bounds.extend(marker.position);
-                                        google.maps.event.addListener(marker, "mousedown", (function(marker, i) {
-                                          return function() {
-                                            infowindow.setContent("<p style='width:95%'><a class='romeluv-google-map-link' href=\"http://postbeschwerde.de/?p=121\"'><b>Von Kundenverhöhnung und verschwundenen Päckchen</b></a><br /> <i>DHL</i>  <br /><b>Sickingenstraße 7, 10553 Berlin </b><br /><br /></p>");
-                                            infowindow.open(map, marker);
-                                          }
-                                        })(marker, i));
-                                        
-                                          
+  <div id="romeluv-global-map" style="width:100%; height:520px; "></div>
+
+  <script type="text/javascript">
+
+    var map = new google.maps.Map(document.getElementById("romeluv-global-map"), {
+      zoom: 17,
+
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+    var bounds = new google.maps.LatLngBounds();
+    var marker, i;
+    var myIcon = new google.maps.MarkerImage("http://maps.google.com/mapfiles/kml/pal3/icon34.png", null, null, null, new google.maps.Size(30,30));
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(52.53269103999427, 13.326115608215332),
+      map: map,
+      icon:  myIcon
+    });
+    bounds.extend(marker.position);
+    google.maps.event.addListener(marker, "mousedown", (function(marker, i) {
+      return function() {
+        infowindow.setContent("<p style='width:95%'><a class='romeluv-google-map-link' href=\"http://postbeschwerde.de/?p=121\"'><b>Von Kundenverhöhnung und verschwundenen Päckchen</b></a><br /> <i>DHL</i>  <br /><b>Sickingenstraße 7, 10553 Berlin </b><br /><br /></p>");
+        infowindow.open(map, marker);
+      }
+    })(marker, i));
+
+
        //  Fit these bounds to the map
-    map.fitBounds(bounds);
+       map.fitBounds(bounds);
      </script>
 
 
-</div> <!-- end #main-content -->
+   </div> <!-- end #main-content -->
 
 
 
 
-<?php get_footer(); ?>
+   <?php get_footer(); ?>
