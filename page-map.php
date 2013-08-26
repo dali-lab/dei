f6b5739ee2eb4bdcdb67de21eddae38500a4710b
Robin Wang <robin@Robins-MacBook-Air.local>
2013-08-25 23:30:05 -0400

    map

--- page-map.php
+++ page-map.php
@@ -22,7 +22,52 @@ Template Name: Map Page
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 
+
+  var lineCoordinates = [
+    new google.maps.LatLng(43.703223, -72.288576),
+    new google.maps.LatLng(43.703223, -72.988576)
+  ];
+
+  var lineSymbol = {
+    path: google.maps.SymbolPath.CIRCLE,
+    scale: 8,
+    strokeColor: '#393'
+  };
+
+  line = new google.maps.Polyline({
+    path: lineCoordinates,
+    icons: [{
+      icon: lineSymbol,
+      offset: '100%'
+    }],
+    map: map
+  });
+
+  animateCircle();
+}
+
+function animateCircle() {
+    var count = 0;
+    window.setInterval(function() {
+      count = (count + 1) % 200;
+
+      var icons = line.get('icons');
+      icons[0].offset = (count / 2) + '%';
+      line.set('icons', icons);
+  }, 20);
+
+
+
+
+
+
+
+
+
+
 google.maps.event.addDomListener(window, 'load', initialize);
+
+}
      </script>
 
 
