1f6228ca6fe5d1ee6e2cc69f8ca0d981d12f8505
Robin Wang <robin@Robins-MacBook-Air.local>
2013-08-25 23:20:31 -0400

    map

--- page-map.php
+++ page-map.php
@@ -21,23 +21,8 @@ Template Name: Map Page
       center: new google.maps.LatLng(43.703223, -72.288576),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-    var bounds = new google.maps.LatLngBounds();
 
-    var infowindow = new google.maps.InfoWindow();
-    
-    var marker, i;
-
-    bounds.extend(marker.position);
-    google.maps.event.addListener(marker, "mousedown", (function(marker, i) {
-      return function() {
-        infowindow.setContent("<p style='width:95%'><a class='romeluv-google-map-link' href=\"http://postbeschwerde.de/?p=121\"'><b>Von Kundenverhöhnung und verschwundenen Päckchen</b></a><br /> <i>DHL</i>  <br /><b>Sickingenstraße 7, 10553 Berlin </b><br /><br /></p>");
-        infowindow.open(map, marker);
-      }
-    })(marker, i));
-
-
-       //  Fit these bounds to the map
-       map.fitBounds(bounds);
+google.maps.event.addDomListener(window, 'load', initialize);
      </script>
 
 
