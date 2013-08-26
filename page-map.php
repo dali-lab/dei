0879bf4c117a4eec881e592b0b3d636197ebfcbd
Robin Wang <robin@Robins-MacBook-Air.local>
2013-08-26 06:19:25 -0400

    add

--- page-map.php
+++ page-map.php
@@ -12,12 +12,6 @@ Template Name: Map Page
 
    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
 
-   <div id="panel" style="float:right;">
-      <input onclick="clearOverlays();" type=button value="Hide Overlays">
-      <input onclick="showOverlays();" type=button value="Show All Overlays">
-      <input onclick="deleteOverlays();" type=button value="Delete Overlays">
-    </div>
-
   <div id="global-map" style="width:960px; height:520px;"></div>
 
   <script type="text/javascript">
@@ -77,6 +71,13 @@ google.maps.event.addDomListener(window, 'load', initialize);
      </script>
 
 
+ <div id="panel" style="float:right;">
+      <input onclick="clearOverlays();" type=button value="Hide Overlays">
+      <input onclick="showOverlays();" type=button value="Show All Overlays">
+      <input onclick="deleteOverlays();" type=button value="Delete Overlays">
+    </div>
+
+    
    </div> <!-- end #main-content -->
 
 
