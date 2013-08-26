5183b039f9aeba5c4eef5d4ebf301f1675a9d326
Robin Wang <robin@Robins-MacBook-Air.local>
2013-08-26 05:23:20 -0400

    map

--- page-map.php
+++ page-map.php
@@ -12,13 +12,15 @@ Template Name: Map Page
 
    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
 
-   <div id="panel">
+  
+
+  <div id="global-map" style="width:960px; height:520px;">
+     <div id="panel">
       <input onclick="clearOverlays();" type=button value="Hide Overlays">
       <input onclick="showOverlays();" type=button value="Show All Overlays">
       <input onclick="deleteOverlays();" type=button value="Delete Overlays">
     </div>
-
-  <div id="global-map" style="width:960px; height:520px;"></div>
+  </div>
 
   <script type="text/javascript">
 
@@ -62,6 +64,14 @@ function animateCircle() {
       icons[0].offset = (count / 2) + '%';
       line.set('icons', icons);
   }, 20);
+}
+
+function showOverlays() {
+  setAllMap(map);
+}
+
+function clearOverlays() {
+  setAllMap(null);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
