2be9423b3c2b67c58c2e9094dd8c1a85f50aaab3
Robin Wang <robin@Robins-MacBook-Air.local>
2013-09-07 17:43:42 -0400

    tabs

--- home.php
+++ home.php
@@ -7,7 +7,7 @@
 <?php if ( get_option('nova_blog_style') == 'false' ) { ?>
 
 
-<hr style= "align:center; width:90%; border: 1px solid #686c91; margin-bottom:20px;" >
+<hr style= "align:center; width:90%; border: 1px solid #686c91; margin-bottom:30px;" >
 
 <div id="category-name"> 
 
@@ -19,11 +19,11 @@
 		<h4><p style="text-decoration:underline; text-align:center; padding-bottom:75px; color:#126a52; font-size:20px;">Choose an icon below for more information</p></h4>
 
 
-		<hr style= "align:center; width:90%; border: 1px solid #686c91; margin-bottom:60px; margin-top:-60px;" >
+		<hr style= "align:center; width:90%; border: 1px solid #686c91; margin-bottom:70px; margin-top:-60px;" >
 
 	</div>
 
-	<hr style= "position:absolute; margin-top:105px;align:center; width:90%; border: 1px solid #686c91; margin-bottom:20px;" >
+	<hr style= "position:absolute; margin-top:105px;align:center; width:100%; border: 1px solid #686c91; margin-bottom:20px;" >
 
 	<div id="main-area">
 
@@ -31,7 +31,7 @@
 
 
 		<div class="container clearfix">
-			<div id="services" style="margin-top:-30px;">
+			<div id="services" style="margin-top:-60px;">
 				<ul id="main-tabs">
 
 
